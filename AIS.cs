41a05ebcad7b9e7aad0857817cc828c1bb1f360b
Shingokun <hoanglong0338@gmail.com>
2022-12-06 21:42:14 +0700

    add AIS.cs

---
+++ AIS.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Animal_Informations_System
+
+{
+    public class AIS
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
